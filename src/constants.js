b9765cd2c209ba259673950f2cf5fe74bec29544
René Kooi <rene@kooi.me>
2016-03-21 09:57:22 +0100

    uncleverisationise constants definitions

--- src/constants.js
+++ src/constants.js
@@ -1,18 +1,44 @@
-import assign from 'object-assign'
+export const ROLE = {
+  NONE: 0,
+  DJ: 1,
+  BOUNCER: 2,
+  MANAGER: 3,
+  COHOST: 4,
+  HOST: 5,
 
-// turns a string of words into an object of { WORD: number }
-const enumish = (list, start = 0) => list.split(' ')
-  .reduce((o, name, i) => assign(o, { [name]: start + i }), {})
+  AMBASSADOR: 3,
+  ADMIN: 5
+}
 
-export const ROLE = assign(enumish('NONE DJ BOUNCER MANAGER COHOST HOST')
-                          , { AMBASSADOR: 3, ADMIN: 5 })
+export const MEDIA_SOURCE = {
+  YOUTUBE: 1,
+  SOUNDCLOUD: 2
+}
 
-export const MEDIA_SOURCE = enumish('YOUTUBE SOUNDCLOUD', 1)
+export const BAN_DURATION = {
+  HOUR: 'h',
+  DAY: 'd',
+  PERMA: 'f'
+}
 
-export const BAN_DURATION = { HOUR: 'h', DAY: 'd', PERMA: 'f' }
-export const BAN_REASON =
-  enumish('SPAMMING VERBAL_ABUSE OFFENSIVE_PLAYS GENRE ATTITUDE', 1)
+export const BAN_REASON = {
+  SPAMMING: 1,
+  VERBAL_ABUSE: 2,
+  OFFENSIVE_PLAYS: 3,
+  GENRE: 4,
+  ATTITUDE: 5
+}
 
-export const MUTE_DURATION = { SHORT: 's', MEDIUM: 'm', LONG: 'l' }
-export const MUTE_REASON =
-  enumish('VIOLATING_RULES VERBAL_ABUSE SPAMMING LANGUAGE ATTITUDE', 1)
+export const MUTE_DURATION = {
+  SHORT: 's',
+  MEDIUM: 'm',
+  LONG: 'l'
+}
+
+export const MUTE_REASON = {
+  VIOLATING_RULES: 1,
+  VERBAL_ABUSE: 2,
+  SPAMMING: 3,
+  LANGUAGE: 4,
+  ATTITUDE: 5
+}
