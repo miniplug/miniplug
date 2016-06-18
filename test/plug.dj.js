1451909ed48f4ea2e135228d38cd0a6fdf3f27d9
René Kooi <renee@kooi.me>
2016-06-18 13:36:06 +0200

    add travis

---
+++ test/plug.dj.js
@@ -0,0 +1,15 @@
+import request from 'request'
+
+describe('plug.dj', function () {
+
+  it('is reachable', done => {
+    request('https://plug.dj/', (e, resp, body) => {
+      if (e)
+        throw e
+      if (body.indexOf('<title>maintenance') !== -1)
+        throw new Error('plug.dj is currently in maintenance mode.')
+      done()
+    })
+  })
+
+})
