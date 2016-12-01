008b198620f6c6145e9d61a83a5165f2e645d791
René Kooi <renee@kooi.me>
2016-12-01 12:35:52 +0100

    Use builtin-modules for rollup externals

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,15 @@
 import commonjs from 'rollup-plugin-commonjs'
 import resolve from 'rollup-plugin-node-resolve'
+import builtins from 'builtin-modules'
+
+const { dependencies } = require('./package.json')
 
 export default {
   entry: 'es/index.js',
   format: 'cjs',
   dest: 'index.js',
   exports: 'default',
-  external: [ 'events', 'querystring' ].concat(
-    Object.keys(require('./package.json').dependencies)
-  ),
+  external: [ ...builtins, ...Object.keys(dependencies) ],
   plugins: [
     commonjs({
       include: 'node_modules/**'
