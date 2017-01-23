d9549a5a002c25e054e1b4bd1a377ff444fcb403
René Kooi <renee@kooi.me>
2017-01-23 18:47:20 +0100

    Fix Media prop on HistoryEntries.

--- src/data/historyEntry.js
+++ src/data/historyEntry.js
@@ -9,7 +9,7 @@ export default function wrapHistoryEntry (mp, raw) {
   const entry = {
     id: raw.id,
     // wrapMedia expects a playlist ID, but we don't know it--pass null instead.
-    media: wrapMedia(mp, null, raw),
+    media: wrapMedia(mp, null, raw.media),
     room: wrapRoom(mp, raw.room),
     user: wrapUser(mp, raw.user),
     time: parseDate(raw.timestamp),
