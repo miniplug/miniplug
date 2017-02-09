96cb2772699af4646862bc4e8561ab44c6be68d2
René Kooi <renee@kooi.me>
2017-02-09 19:02:01 +0100

    Support getting history for other users, fixes #25

--- src/plugins/history.js
+++ src/plugins/history.js
@@ -8,8 +8,20 @@ export default function historyPlugin () {
     const getRoomHistory = () =>
       mp.get('rooms/history').map(wrapHistoryEntry)
 
-    const getUserHistory = () =>
+    // Get the history for the logged-in user.
+    const getOwnHistory = () =>
       mp.get('users/me/history').map(wrapHistoryEntry)
+
+    const getProfile = (id) =>
+      mp.get(`profile/${id}`).get(0)
+
+    // Get the history for another user.
+    const getOtherHistory = (id) =>
+      getProfile(id).get('history').map(wrapHistoryEntry)
+
+    // Get the history for a user. Defaults to the current user.
+    const getUserHistory = (id = mp.me().id) =>
+      mp.me().id === id ? getOwnHistory() : getOtherHistory(id)
 
     Object.assign(mp, {
       getRoomHistory,
