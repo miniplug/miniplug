1fbf7513c94fa3f7466ecba2256a276455b7ab0b
René Kooi <renee@kooi.me>
2017-03-23 17:26:47 +0100

    Implement `maintenance` and `maintenanceAlert` events.

--- src/plugins/system.js
+++ src/plugins/system.js
@@ -4,8 +4,18 @@ export default function systemPlugin () {
       mp.emit('systemMessage', text)
     }
 
+    function onMaintenanceAlert (minutesLeft) {
+      mp.emit('maintenanceAlert', minutesLeft)
+    }
+
+    function onMaintenance () {
+      mp.emit('maintenance')
+    }
+
     mp.on('connected', () => {
       mp.ws.on('plugMessage', onMessage)
+      mp.ws.on('plugMaintenanceAlert', onMaintenanceAlert)
+      mp.ws.on('plugMaintenance', onMaintenance)
     })
   }
 }
