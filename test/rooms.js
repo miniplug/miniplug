d3b3d8d7694f9ae2c75e110fb006ce3069762eaa
René Kooi <renee@kooi.me>
2017-03-27 23:53:08 +0200

    Add tests for room joining.

--- test/rooms.js
+++ test/rooms.js
@@ -11,3 +11,30 @@ test('Getting rooms by the current user', async (t) => {
   t.equal(rooms.length, 5)
   t.equal(rooms[0].name, '`plug-socket` testing room')
 })
+
+test('Sets the current room after joining', async (t) => {
+  t.plan(3)
+
+  nock.post('/_/rooms/join').reply(200, require('./mocks/rooms/join.json'))
+  nock.get('/_/rooms/state').reply(200, require('./mocks/rooms/state.json'))
+
+  const mp = await miniplug()
+
+  await mp.join('tastycat')
+  t.ok(mp.room())
+  t.equal(mp.room().slug, 'tastycat')
+
+  t.equal(mp.users().length, 153)
+})
+
+test('Emits `roomState` after receiving new room state', async (t) => {
+  t.plan(1)
+
+  nock.get('/_/rooms/state').reply(200, require('./mocks/rooms/state.json'))
+
+  const mp = miniplug()
+  mp.on('roomState', () => {
+    t.pass()
+  })
+  const state = await mp.getRoomState()
+})
