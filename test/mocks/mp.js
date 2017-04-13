91e45c0aa57e100807c29a9b7dee7f7c1974c8f7
René Kooi <renee@kooi.me>
2017-04-13 09:32:32 +0200

    test: Implement mocking for Socket events.

--- test/mocks/mp.js
+++ test/mocks/mp.js
@@ -1,14 +1,38 @@
+const EventEmitter = require('events').EventEmitter
 const Promise = require('bluebird')
 const miniplug = require('../../')
+const socket = require('proxyquire')('plug-socket', {
+  // A not-WebSocket that is a good enough imitation for plug-socket
+  ws: function WebSocket () {
+    const fakeSocket = new EventEmitter()
+    setImmediate(() => {
+      fakeSocket.onopen && fakeSocket.onopen()
+    })
+
+    // Adding a thing so tests can mock socket events more easily
+    fakeSocket.receiveMessages = (messages) => {
+      fakeSocket.onmessage({
+        data: JSON.stringify(messages)
+      })
+    }
+
+    return fakeSocket
+  }
+})
 
 module.exports = () => {
   const mp = miniplug({
     connect: false
   })
 
+  mp.ws = socket()
+
   mp.connected = Promise.resolve({
     cookie: 'Fake Cookie'
   })
+
+  // TODO make this configurable by the calling test
+  mp.emit('connected', { id: 123456, guest: true })
 
   return mp
 }
