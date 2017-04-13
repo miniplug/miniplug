8e2ef3532f1101ae7bcb26b8712ca7a9323ab831
René Kooi <renee@kooi.me>
2017-04-13 09:41:14 +0200

    Add tests for `user()` method.

--- test/users.js
+++ test/users.js
@@ -19,3 +19,21 @@ test('Get the current user', async (t) => {
   const user = await miniplug().getMe()
   t.equal(user.username, 'ReAnna')
 })
+
+test('Get a user who is currently in the room', async (t) => {
+  t.plan(3)
+
+  nock.post('/_/rooms/join').reply(200, require('./mocks/rooms/join.json'))
+  nock.get('/_/rooms/state').reply(200, require('./mocks/rooms/state.json'))
+
+  const mp = await miniplug()
+  await mp.join('tastycat')
+
+  mp.emit('connected', {
+    id: 555555
+  })
+
+  t.ok(mp.user(4103894), 'should return user who is in the room')
+  t.notok(mp.user(123456), 'should not return user who is not in the room')
+  t.ok(mp.user(555555), 'should get the current user, issue #35')
+})
