3be3291ba59331ceb651b4f60372f8f5903ba1cf
Renée Kooi <renee@kooi.me>
2017-05-08 16:10:02 +0200

    Add BadLoginError and NotEnoughPPError

--- src/errors.js
+++ src/errors.js
@@ -9,13 +9,21 @@ function setup (response, cause) {
   this.cause = cause
 }
 
+function setupFundsPP (response, cause) {
+  setup.call(this, response, cause)
+  this.message = 'You don\'t have enough Plug Points to unlock this.'
+}
+
 export const RequestError = createErrorClass('RequestError', setup)
+export const BadLoginError = createErrorClass('BadLoginError', setup)
 export const NotAuthorizedError = createErrorClass('NotAuthorizedError', setup)
 export const NotFoundError = createErrorClass('NotFoundError', setup)
 export const NoValidPlaylistError = createErrorClass('NoValidPlaylistError', setup)
+export const NotEnoughPPError = createErrorClass('NotEnoughPPError', setupFundsPP)
 
 const statusToError = {
   requestError: RequestError,
+  badLogin: BadLoginError,
   notAuthorized: NotAuthorizedError,
   notFound: NotFoundError,
   noValidPlaylist: NoValidPlaylistError,
@@ -23,6 +31,11 @@ const statusToError = {
 
 export function wrapResponseError(response, cause) {
   const { status, data } = response.body
+  // Special cases
+  if (status === 'requestError' && data && data[0] === 'fundsPP') {
+    return new NotEnoughPPError(response, cause)
+  }
+
   const ErrorClass = statusToError[status]
   if (ErrorClass) {
     return new ErrorClass(response, cause)
