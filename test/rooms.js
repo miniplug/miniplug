ab263b8ba075f588dffa6e1816d8a265e3ffe0e4
Renée Kooi <renee@kooi.me>
2017-05-18 15:10:13 +0200

    test room update events

--- test/rooms.js
+++ test/rooms.js
@@ -38,3 +38,43 @@ test('Emits `roomState` after receiving new room state', async (t) => {
   })
   const state = await mp.getRoomState()
 })
+
+test('Updates `room()` properties when updates come in', async (t) => {
+  t.plan(9)
+  nock.get('/_/rooms/state').reply(200, require('./mocks/rooms/state.json'))
+
+  const mp = miniplug()
+  await mp.getRoomState()
+
+  mp.on('roomDescriptionUpdate', t.pass)
+  mp.on('roomWelcomeUpdate', t.pass)
+  mp.on('roomNameUpdate', t.pass)
+  mp.on('roomMinChatLevelUpdate', t.pass)
+
+  t.true(/Welcome to the Tastycat room/.test(mp.room().description))
+
+  mp.ws.onmessage({
+    data: JSON.stringify([
+      { a: 'roomDescriptionUpdate', p: {
+        u: 4393540,
+        d: 'New description'
+      }, s: 'tastycat' }
+    ])
+  })
+  t.true(/New description/.test(mp.room().description))
+
+  mp.ws.onmessage({
+    data: JSON.stringify([
+      { a: 'roomWelcomeUpdate', p: { u: 4393540, w: 'New welcome' }, s: 'tastycat' },
+      { a: 'roomNameUpdate', p: { u: 4393540, n: 'room name' }, s: 'tastycat' }
+    ])
+  })
+  mp.ws.onmessage({
+    data: JSON.stringify([
+      { a: 'roomMinChatLevelUpdate', p: { u: 123456, m: 2 }, s: 'tastycat' }
+    ])
+  })
+  t.equal(mp.room().welcome, 'New welcome')
+  t.equal(mp.room().name, 'room name')
+  t.equal(mp.room().minChatLevel, 2)
+})
